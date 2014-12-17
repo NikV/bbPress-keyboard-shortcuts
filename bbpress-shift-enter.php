bb9d5f4c6f55ca3a9c6cec25f49200e2851892f8
Slava UA <slaFFik@users.noreply.github.com>
2014-12-17 23:51:49 +0200

    Inline js

--- bbpress-shift-enter.php
+++ bbpress-shift-enter.php
@@ -5,19 +5,22 @@
  * Author: Nikhil Vimal
  * Author URI: http://nik.techvoltz.com
  * Version: 1.0
- * Plugin URI:
+ * Plugin URI: https://github.com/NikV/bbPress-shift-enter/
  * License: GNU GPLv2+
  */
 
-function bbpress_reply_with_shift_enter_enqueue() {
+function bbpress_reply_with_shift_enter_js() {
+	?>
+	<script>
+	document.getElementById("bbpress-forums").onkeydown = function (submit) {
+	        if (submit.keyCode == 13 && submit.shiftKey) {
+	            document.getElementById("bbp_reply_submit").click();
+	            return false;
+	        }
+    	}
+	</script>
+	<?php
+}
 
-		if ( is_singular('topic') ) {
-			//Enqueue the bbPress reply shortcut script (Only in the topic post type)
-			wp_enqueue_script( 'bbpress_reply_keyboard_shortcut', plugin_dir_url( __FILE__ ) . 'bbpress-reply-shift-enter.js' );
-
-		}
-
-
-	}
-add_action('wp_enqueue_scripts','bbpress_reply_with_shift_enter_enqueue');
+add_action( 'wp_footer', 'bbpress_reply_with_shift_enter_js', 99 );
 
