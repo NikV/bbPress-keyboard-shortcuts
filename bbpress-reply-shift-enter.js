3b9f82283135fd086b5405590908e961af912f52
NikV <techvoltz@gmail.com>
2015-01-01 20:11:20 -0600

    CMD+enter shortcut added

--- bbpress-reply-shift-enter.js
+++ bbpress-reply-shift-enter.js
@@ -1,13 +1,15 @@
 /**
  * Javascript file for bbPress Shift+Enter to reply
  */
-
 window.addEventListener = function () {
     document.getElementById("bbpress-forums").onkeydown = function (submit) {
 
         if (submit.keyCode == 13 && submit.shiftKey) {
             document.getElementById("bbp_reply_submit").click();
-            return false;
+        }
+
+        else if (submit.keyCode == 13 && submit.metaKey ) {
+            document.getElementById("bbp_reply_submit").click();
         }
     }
 }
